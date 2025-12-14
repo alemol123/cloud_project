578e18466561324d45d8aa050b02916fb77848e4
Luna Laracher <alemol123@gmail.com>
2025-12-14 17:12:50 +0100

    Update Azure Functions code and fix prepMinutes issues

--- azure-functions/HTTPGetMealsByArea/__init__.py
+++ azure-functions/HTTPGetMealsByArea/__init__.py
@@ -16,23 +16,32 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         table = get_table_client(MEALS_TABLE)
         entities = list(table.query_entities(f"PartitionKey eq '{area}'"))
-    except:
+    except Exception:
+        logging.exception("Error querying Meals table")
         return _error("Failed to fetch meals")
 
     meals = []
     for e in entities:
+
+        meal_name = e.get("name")
+        restaurant = e.get("restaurantName")
+        description = e.get("description")
+        prep_minutes = e.get("prepMinutes") 
+        price = e.get("price")
+
         meals.append({
-            "mealId": e["RowKey"],
-            "restaurantName": e.get("RestaurantName"),
-            "dishName": e.get("DishName"),
-            "description": e.get("Description"),
-            "prepTimeMinutes": e.get("PrepTimeMinutes"),
-            "price": e.get("Price"),
-            "imageUrl": e.get("ImageUrl", "")
+            "id": e["RowKey"],            
+            "mealId": e["RowKey"],        
+            "name": meal_name,
+            "restaurantName": restaurant,
+            "description": description,
+            "prepMinutes": prep_minutes,  
+            "price": price,
+            "imageUrl": e.get("imageUrl", "")
         })
 
     return func.HttpResponse(
-        json.dumps({"status": "ok", "meals": meals}),
+        json.dumps(meals),
         mimetype="application/json"
     )
 
